9abb81fab719f7b136d624ba2985dc5139c6e12a
Alessandro <ale.tallini2000@gmail.com>
2020-06-17 17:21:07 +0200

    finished login system

--- application.py
+++ application.py
@@ -22,11 +22,25 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    session["isloggedin"] = None
+    return render_template("index.html", isloggedin=session["isloggedin"])
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/logedin", methods=["POST"])
+def logedin():
+    error = None
+
+    session["name"] = request.form.get("name")
+    session["password"] = request.form.get("password")
+    session["user"] = db.execute("SELECT * FROM users WHERE (name = :name) AND (password = :password)", {"name": session["name"],"password": session["password"]}).fetchone()
+    if session["user"] == None:
+        error = "Incorrect username or password. Please try again"
+        return render_template("login.html", error=error)
+    session["isloggedin"] = True
+    return render_template("logedin.html", name=session["name"], isloggedin=session["isloggedin"])
 
 @app.route("/signup")
 def signup():
@@ -34,9 +48,17 @@ def signup():
 
 @app.route("/signedup", methods=["POST"])
 def signedup():
+    error = None
+
     name = request.form.get("name")
     password = request.form.get("password")
+    isnamein = db.execute("SELECT * FROM users WHERE name= :name", {"name": name}).rowcount
+
+    if isnamein > 0:
+        error = "The username you chose already exists, please pick a different one."
+        return render_template("signup.html", error=error)
+
     db.execute("INSERT INTO users (name, password) VALUES (:name, :password)",
                   {"name": name, "password": password})
     db.commit()
-    return render_template("signedup.html", name=name, password=password)
+    return render_template("signedup.html", name=name)
