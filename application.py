008cb561585976d39e71d2d8aa8c7261e3403d71
Alessandro <ale.tallini2000@gmail.com>
2020-06-17 14:25:15 +0200

    adding new files

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,7 +20,23 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/signedup", methods=["POST"])
+def signedup():
+    name = request.form.get("name")
+    password = request.form.get("password")
+    db.execute("INSERT INTO users (name, password) VALUES (:name, :password)",
+                  {"name": name, "password": password})
+    db.commit()
+    return render_template("signedup.html", name=name, password=password)
